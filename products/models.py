1d1fbba6c7167ff593e9f77af2346d163dad075e
Lara Mihelin <lamihelin@vsite.hr>
2025-02-26 21:28:38 +0100

    added admin role

--- products/models.py
+++ products/models.py
@@ -6,3 +6,8 @@ class Product(models.Model):
     price = models.FloatField()
     quantity = models.IntegerField()
     image_url = models.CharField(max_length=2083)
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.TextField()
+    discount = models.FloatField()
